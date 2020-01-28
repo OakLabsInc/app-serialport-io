d44a6a7eaec8c429cb90810f7a9281663abb5e2b
Richard Joffray <rjoffray@richards-mbp.lan>
2020-01-28 15:40:04 -0800

    added a readline parser

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 'use strict';
 const SerialPort = require('serialport')
-const port = new SerialPort(process.env.SERIAL_DEVICE)
-
-// Read data that is available but keep the stream in "paused mode"
-port.on('readable', function () {
-  console.log('Data:', port.read())
+const port = new SerialPort(process.env.SERIAL_DEVICE, {
+  baudRate: 56700
 })
 
+// // Read data that is available but keep the stream in "paused mode"
+// port.on('readable', function () {
+//   console.log('Data:', port.read())
+// })
+
+var Readline = SerialPort.parsers.Readline;
+var parser = new Readline()
+
 // Switches the port into "flowing mode"
-port.on('data', function (data) {
+parser.on('data', function (data) {
   console.log('Data:', data.toString())
 })
 
