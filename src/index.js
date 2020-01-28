f612ad892f5159eb3129d859aad9e117be8a035f
Richard Joffray <rjoffray@richards-mbp.lan>
2020-01-28 15:59:07 -0800

    changed to hex data

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ const SerialPort = require('serialport')
 const port = new SerialPort(process.env.SERIAL_DEVICE)
 
 // Read data that is available but keep the stream in "paused mode"
-port.on('readable', function () {
-  console.log('Read Data:', port.read())
-})
+// port.on('readable', function () {
+//   console.log('Read Data:', port.read())
+// })
 
 // Switches the port into "flowing mode"
 port.on('data', function (data) {
-  console.log('Data:', data)
+  console.log('Data:', data.toString('hex'))
 })
 
 // // Pipe the data into another stream (like a parser or standard out)
