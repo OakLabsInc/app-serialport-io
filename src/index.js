f274c8f0d74cc09d4f30e7b4aa3e279d6ad7812c
Richard Joffray <rjoffray@richards-mbp.lan>
2020-01-29 13:50:27 -0800

    changed to hex values

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ const SerialPort = require('serialport')
 const port = new SerialPort(process.env.SERIAL_DEVICE)
 
 port.on('readable', function () {
-  console.log('readable data:', port.read().toString('utf8'))
+  console.log('readable data:', port.read().toString('hex'))
 })
 
 // Switches the port into "flowing mode"
 port.on('data', function (data) {
-  console.log('data:', data.toString('utf8'))
+  console.log('data:', data.toString('hex'))
 })
 
 // Pipe the data into another stream (like a parser or standard out)
