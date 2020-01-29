283d5eddbed122c0162866d9fd91007ba6e80495
Richard Joffray <rjoffray@richards-mbp.lan>
2020-01-28 16:10:10 -0800

    changing to readable

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ const SerialPort = require('serialport')
 const port = new SerialPort(process.env.SERIAL_DEVICE)
 
 // Read data that is available but keep the stream in "paused mode"
-// port.on('readable', function () {
-//   console.log('Read Data:', port.read())
-// })
-
-// Switches the port into "flowing mode"
-port.on('data', function (data) {
-  console.log('Data:', data.toString('hex'))
+port.on('readable', function () {
+  console.log('Read Data:', port.read().toString())
 })
+
+// // Switches the port into "flowing mode"
+// port.on('data', function (data) {
+//   console.log('Data:', data.toString('hex'))
+// })
 
 // // Pipe the data into another stream (like a parser or standard out)
 // const lineStream = port.pipe(new Readline())
