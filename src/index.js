c7d5a1e3589da6d42026f6786b6f27cc49e3069a
Richard Joffray <rjoffray@richards-mbp.lan>
2020-01-29 14:59:51 -0800

    added parser

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
-const SerialPort = require('serialport')
-const port = new SerialPort(process.env.SERIAL_DEVICE)
 
-// port.on('readable', function () {
-//   console.log('readable data:', port.read().toString('hex'))
-// })
+const SerialPort = require('serialport');
+const Readline = require('@serialport/parser-readline')
 
-// Switches the port into "flowing mode"
-port.on('data', function (data) {
-  console.log('data:', data.toString('hex'))
-})
+var port = new SerialPort(process.env.SERIAL_DEVICE, {
+    baudRate: 57600
+});
 
-// Pipe the data into another stream (like a parser or standard out)
-// const lineStream = port.pipe(new Readline())
+const parser = port.pipe(new Readline({ encoding: 'hex' }))
 
+port.on('data', data => console.log({ port: data.toString('hex') }))
+parser.on('data', data => console.log({ parser: data }))
