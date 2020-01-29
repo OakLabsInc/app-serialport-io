60ef91c04be1673cc772a919ee67dfafbb581356
Richard Joffray <rjoffray@richards-mbp.lan>
2020-01-28 16:24:29 -0800

    set parser

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 'use strict';
 const SerialPort = require('serialport')
-const port = new SerialPort(process.env.SERIAL_DEVICE)
+const port = new SerialPort(process.env.SERIAL_DEVICE, {
+  parser: serialport.parsers.readline('\n'),
+  baudrate: 57600
+})
 
 // Read data that is available but keep the stream in "paused mode"
 port.on('readable', function () {
   console.log('Read Data:', port.read().toString('hex'))
 })
 
-// // Switches the port into "flowing mode"
-// port.on('data', function (data) {
-//   console.log('Data:', data.toString('hex'))
-// })
+// Switches the port into "flowing mode"
+port.on('data', function (data) {
+  console.log('Data:', data.toString('hex'))
+})
 
 // // Pipe the data into another stream (like a parser or standard out)
 // const lineStream = port.pipe(new Readline())
