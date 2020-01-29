79acf08b981547b9a098aafa06648ac98c6d357a
Richard Joffray <rjoffray@richards-mbp.lan>
2020-01-29 15:54:08 -0800

    added a parser

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-
 const SerialPort = require('serialport');
 const Readline = require('@serialport/parser-readline')
 
@@ -6,13 +5,5 @@ var port = new SerialPort(process.env.SERIAL_DEVICE, {
     baudRate: 57600
 });
 
-const parser = port.pipe(new Readline({ encoding: 'hex' }))
-
-port.on('data', data => console.log({ port: hexToString(data.toString('hex')) }))
+const parser = port.pipe(new Readline({ delimiter: '\r', encoding: 'utf8' }))
 parser.on('data', data => console.log({ parser: data }))
-
-function hexToString(str)
-{
-    const buf = new Buffer(str, 'hex');
-    return buf.toString('utf8');
-}
