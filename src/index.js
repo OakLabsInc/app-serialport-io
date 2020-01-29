693476b18ba52a868be4122932c7f66c1f64121e
Richard Joffray <rjoffray@richards-mbp.lan>
2020-01-28 17:39:59 -0800

    back to basics

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 const SerialPort = require('serialport')
-const Readline = require('@serialport/parser-readline')
 const port = new SerialPort(process.env.SERIAL_DEVICE)
-const parser = new Readline()
-port.pipe(parser)
-parser.on('data', console.log)
+
+port.on('readable', function () {
+  console.log('readable data:', port.read().toString('utf8'))
+})
+
+// Switches the port into "flowing mode"
+port.on('data', function (data) {
+  console.log('data:', data.toString('utf8'))
+})
+
+// Pipe the data into another stream (like a parser or standard out)
+const lineStream = port.pipe(new Readline())
 
