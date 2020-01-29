3687edbc507453837c15e4a3f2c31e440c2473f7
Richard Joffray <rjoffray@richards-mbp.lan>
2020-01-29 15:27:02 -0800

    hex to string

--- src/index.js
+++ src/index.js
@@ -8,5 +8,11 @@ var port = new SerialPort(process.env.SERIAL_DEVICE, {
 
 const parser = port.pipe(new Readline({ encoding: 'hex' }))
 
-port.on('data', data => console.log({ port: data.toString('hex') }))
+port.on('data', data => console.log({ port: hexToString(data.toString('hex')) }))
 parser.on('data', data => console.log({ parser: data }))
+
+function hexToString(str)
+{
+    const buf = new Buffer(str, 'hex');
+    return buf.toString('utf8');
+}
