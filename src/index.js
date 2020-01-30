85ff47457e8036ef686b4b09ab528e049e019835
Richard Joffray <rjoffray@richards-mbp.lan>
2020-01-29 16:11:33 -0800

    add data output limitation

--- src/index.js
+++ src/index.js
@@ -5,10 +5,16 @@ var port = new SerialPort(process.env.SERIAL_DEVICE, {
     baudRate: 57600
 });
 var output = ""
+// Setup the parser for the expected output format
 const parser = port.pipe(new Readline({ delimiter: '\r', encoding: 'utf8' }))
 
 parser.on('data', data => {
+
+    // only output or take action if the data has changed
     if (data !== output){
-        console.log({ parser: data })
+        // set new data output
+        output = data
+        console.log({ parser: output })
     }
+    
 })
