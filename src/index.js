ab70cdf41c84daac6bf706c8e74621e44be8e267
Richard Joffray <rjoffray@richards-mbp.lan>
2020-01-29 16:07:01 -0800

    add change detection

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ const Readline = require('@serialport/parser-readline')
 var port = new SerialPort(process.env.SERIAL_DEVICE, {
     baudRate: 57600
 });
-
+var output = ""
 const parser = port.pipe(new Readline({ delimiter: '\r', encoding: 'utf8' }))
-parser.on('data', data => console.log({ parser: data }))
+
+parser.on('data', data => {
+    if (data !== output){
+        console.log({ parser: data })
+    }
+})
